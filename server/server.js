8f6f615aff2ac97c2c0343e5a64cb0f995d61d13
Américo <americochiclana@gmail.com>
2023-06-19 17:21:58 +0200

    terminado api

--- server/server.js
+++ server/server.js
@@ -1,8 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express()
 
-app.get('/productos', (req,res) => res.send({message: 'Prueba'}));
+//DB connection
+mongoose.connect('mongodb://localhost/Garaje')
+    .then(() => console.log('CONECTADO A BBDD'))
+
+//Model
+const Coaster = require('./models/coaster.model');
+
+
+//Routing
+app.get('/api/coaster', (req,res) => {
+
+    Coaster
+        .find()
+        .then(allCoasters => res.send(allCoasters));
+});
+
+app.get('/api/details/:coaster_id', (req,res) => {
+
+    const {coaster_id} = req.params
+
+    Coaster
+        .findById(coaster_id)
+        .then(coaster => res.send(coaster))
+});
 
 
 app.listen(5005, () => console.log('SERVIDOR LEVANTADO')) 
+
